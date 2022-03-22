6f95c25bb794562b9d09b60535c13d75857c8f02
Jonathan Cunnane <jonathan.cunnane@gmail.com>
2022-03-22 13:26:13 +0000

    Basic window setup and graphing a function.

--- Complex_Loci.py
+++ Complex_Loci.py
@@ -2,5 +2,8 @@ import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
     plt.show()
-    plt.axis([0, 50, 0, 50])
+    x = list(range(-20, 20))
+    y = [0.3*num**3-num**2+2*(num)+15 for num in x]
+    plt.axis([min(x), max(x), min(y), max(y)])
+    plt.plot(x, y)
     plt.show()
