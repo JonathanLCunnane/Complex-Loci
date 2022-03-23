a73690a77854eb70463ec3f554cad5d8e5f9f456
JonathanLCunnane <jonathan.cunnane@gmail.com>
2022-03-23 22:51:14 +0000

    user can now enter circle radius and center (for testing purposes)

--- Complex_Loci.py
+++ Complex_Loci.py
@@ -1,12 +1,15 @@
+from turtle import ycor
 import matplotlib.pyplot as plt
 from graphing import GraphingBrush as gbrush
 
 
 def main():
+    radius = float(input("Enter circle radius: "))
+    x, y = [float(i) for i in input("Enter the center coordinates in the form 'x, y': ").split(",")]
     default_brush = gbrush(plt)
     default_brush.one_to_one_aspect()
+    default_brush.circle(radius, (x, y))
     default_brush.draw_graph_lines()
-    default_brush.circle(3, (1, 2))
     plt.title("Complex Loci Plot")
     plt.show()
     
