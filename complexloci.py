ec16b7387bc36539e424083abd47b3dd81bd8910
Jonathan Cunnane <jonathan.cunnane@gmail.com>
2022-03-25 18:42:14 +0000

    Started on tkinter gui implementation for inputs

--- complexloci.py
+++ complexloci.py
@@ -1,10 +1,38 @@
 import matplotlib.pyplot as plt
-from matplotlib.widgets import TextBox
+import tkinter as tk
 from graphing import GraphingBrush as gbrush
 from graphing import setup_figure
 
+class Window(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        # Setup tkinter window
+        self.title("Complex Loci Plotter")
+        self.iconphoto(True, tk.PhotoImage(file=r"./icon.png"))
+        self.geometry("960x540")
+
+        # Setup tkinter vars
+        self.entrytext = tk.StringVar()
+
+        # Setup widgets
+        self.widget_setup()
+
+    def widget_setup(self):
+        padding = {"padx" : 10, "pady": 10}
+        entry = tk.Entry(self, justify="right", validate="all", textvariable=self.entrytext, width=20, font=50)
+        entry.grid(column=0, row=0, **padding)
+        self.entrytext.trace_add("write", self.on_entry_change)
+
+    def on_entry_change(self, *args):
+        print(self.entrytext.get())
+
+
 
 def main():
+    # Start tkinter window
+    window = Window()
+    window.mainloop()
+
     # Temporary Inputs for testing purposes
     radius = float(input("Enter circle radius: "))
     x, y = [float(i) for i in input("Enter the center coordinates in the form 'x, y': ").split(",")]
@@ -16,9 +44,6 @@ def main():
     # Creating and using brush
     default_brush = gbrush(plt, figure, axes)
     default_brush.circle(radius, (x, y))
-    
-    axbox = plt.axes([0.1, 0.05, 0.8, 0.075])
-    text_box = TextBox(axbox, 'Enter Locus Equation: ', initial=f"|z-({x}+{y}i)|={radius}")
 
     # Set title
     plt.title("Complex Loci Plot")
