34d4a39db678d14f00936db8877269655240c127
JonathanLCunnane <jonathan.cunnane@gmail.com>
2022-04-09 15:26:50 +0100

    Circle loci are completely functional and error free now.

--- interpreting.py
+++ interpreting.py
@@ -1,3 +1,12 @@
+def __circle(params: tuple[str, dict[str, float]]):
+    """
+    A post input parsing check, to check if the circle's parameters are valid.
+    """
+    if params[1]["radius"] <= 0 or params[1]["radius"] >= 1073741824 or params[1]["center"][0] >= 1073741824 or params[1]["center"][1] >= 1073741824 or params[1]["center"][0] <= -1073741824 or params[1]["center"][1] <= -1073741824:
+        return None
+    else:
+        return params
+
 def circle_locus(regexgroups: tuple[str]) -> tuple[str, dict[str, float]]:
     center = [0, 0]
     radius = 1
@@ -7,8 +16,10 @@ def circle_locus(regexgroups: tuple[str]) -> tuple[str, dict[str, float]]:
         if regexgroups[13][:3] == "|z|":
             center = [0, 0]
             radius = float(regexgroups[14])
+            circle = ("circle", {"radius": radius, "center": tuple(center)})
+            return __circle(circle)
     # if in the form |z \pm *|
-    elif regexgroups[1] != None:
+    if regexgroups[1] != None:
         if regexgroups[1].startswith("z"):
             # taking into account the sign after z
             if regexgroups[8] == "-":
@@ -31,26 +42,27 @@ def circle_locus(regexgroups: tuple[str]) -> tuple[str, dict[str, float]]:
                 else:
                     center[1] = coeff*-float(y)
             radius = float(regexgroups[12])
+            circle = ("circle", {"radius": radius, "center": tuple(center)})
+            return __circle(circle)
         # if in the form |* \pm z|
-    else:
-        # if in the form |* p z|
-        if regexgroups[1][-2] == "-":
-            coeff = 1
-        # if in the form |* m z|
         else:
-            coeff = -1
-        if regexgroups[4] != None: 
-            center[0] = coeff*float(regexgroups[4])
-        if regexgroups[5] != None:
-            y = regexgroups[5][:-1]
-            if y == "" or y == "+":
-                center[1] = coeff * 1
-            elif y == "-":
-                center[1] = coeff * -1
+            # if in the form |* p z|
+            if regexgroups[1][-2] == "-":
+                coeff = 1
+            # if in the form |* m z|
             else:
-                center[1] = coeff*float(y)
-        radius = float(regexgroups[12])
-    # if radius is zero
-    if radius <= 0 or radius >= 1073741824 or center[0] >= 1073741824 or center[1] >= 1073741824 or center[0] <= -1073741824 or center[1] <= -1073741824:
-        return None
-    return ("circle", {"radius": radius, "center": tuple(center)})
+                coeff = -1
+            if regexgroups[4] != None: 
+                center[0] = coeff*float(regexgroups[4])
+            if regexgroups[5] != None:
+                y = regexgroups[5][:-1]
+                if y == "" or y == "+":
+                    center[1] = coeff * 1
+                elif y == "-":
+                    center[1] = coeff * -1
+                else:
+                    center[1] = coeff*float(y)
+            radius = float(regexgroups[12])
+            circle = ("circle", {"radius": radius, "center": tuple(center)})
+            return __circle(circle)
+    return None
